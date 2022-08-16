c6212a2c6842a2481bae5e9e303ea8e1fbe6ae15
vinhho3010 <anhvinh30102001@gmail.com>
2022-08-16 16:24:28 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
+const contactsRouter = require("./app/routes/contact.route");
 
 app.use(cors());
 app.use(express.json());
@@ -9,5 +10,7 @@ app.use(express.json());
 app.get("/", (req, res)=>{
     res.json({ message: "Welcome to contact book application." });
 });
+
+app.use("/api/contacts", contactsRouter);
 
 module.exports=app;
